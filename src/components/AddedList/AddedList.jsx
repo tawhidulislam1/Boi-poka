2203dff4ea87729d342829bfc5f0494ae9adbf89
unknown <designwithtawhid@gmail.com>
2024-11-02 15:09:59 +0600

    update list page

--- src/components/AddedList/AddedList.jsx
+++ src/components/AddedList/AddedList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const AddedList = ({ book }) => {
-    const { image, bookName, author , bookId} = book;
+    const { image, bookName, author, bookId, tags, publisher,totalPages } = book;
     return (
         <Link to={`/books/${bookId}`}>
             <div className='flex items-center gap-3 bg-slate-900 border-2 my-5 justify-center py-5'>
@@ -10,6 +10,18 @@ const AddedList = ({ book }) => {
                 <div>
                     <h3>{bookName}</h3>
                     <h3>{author}</h3>
+                    <div className='flex justify-start items-center gap-2 my-2'>
+                        <h2 className='font-bold'>Tags:</h2>{
+                            tags.map((tag, idx) => <button key={idx} className="btn btn-sm btn-error">{tag}</button>)
+                        }
+                    </div>
+                    <div className="divider"></div>
+                    <div className='flex justify-between gap-2'>
+                    <h2>@publisher: {publisher}</h2>
+                    <h2>@Total Pages: {totalPages}</h2>
+
+                    </div>
+                   
                 </div>
             </div>
         </Link>
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,9 +8,9 @@ const Banner = () => {
                     src={bannerImg}
                     className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <h1 className="text-5xl font-bold">Books to freshen up your bookshelf</h1>
                   
-                    <button className="btn btn-primary">Get Started</button>
+                    <button className="btn btn-primary">View The List</button>
                 </div>
             </div>
         </div>
